d97b06dbd044592b554fa17afdcbdccc6baadfa5
Muataz Aziz <muataz.aziz91@yahoo.com>
2017-01-10 21:16:00 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,6 +1,6 @@
 
 def hopper
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -15,14 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:grace_hopper] #this is is part of sulotion
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -37,10 +35,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for] #this is is part of sulotion
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -55,6 +54,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages][0] #this is is part of sulotion
+
 end
 
 def adding_matz
@@ -64,7 +65,8 @@ def adding_matz
 #    :languages => ["LISP, C"]
 # }
 
-	programmer_hash = 
+
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -80,11 +82,14 @@ def adding_matz
         }
      }
 
-    
+		 programmer_hash[:yukihiro_matsumoto]={    #this is is part of sulotion
+		   :known_for => "Ruby",                   #this is is part of sulotion
+		   :languages => ["LISP, C"]					}    #this is is part of sulotion
+			 programmer_hash                         #this is is part of sulotion
 end
 
 def changing_alan
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -99,14 +104,14 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for the value of the alans_new_info variable. 
+     #change what Alan Kay is :known_for the value of the alans_new_info variable.
      alans_new_info = "GUI"
-     
-     
+programmer_hash[:alan_kay][:known_for]="GUI" #this is is part of sulotion
+programmer_hash                              #this is is part of sulotion
 end
 
 def adding_to_dennis
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -121,19 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-     
+		 programmer_hash[:dennis_ritchie][:languages]<<"Assembly" #this is is part of sulotion
+     programmer_hash                                          #this is is part of sulotion
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
